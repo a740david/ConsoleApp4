46684de2c40073739790fcd3e006fc0ee94f0c52
a740david <54502632+a740david@users.noreply.github.com>
2019-11-25 19:40:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class GuestRe
+    {
+        private int EntryDate, ReleaseDate;
+        bool IsApproved;
+        public int X
+        {
+            get { return EntryDate; }
+            set { EntryDate = value; }
+        }
+        public int y
+        {
+            get { return ReleaseDate; }
+            set {ReleaseDate = value; }
+        }
+        public bool z
+            get { return IsApproved; }
+            set { =IsApproved true; }
+    }
+}
